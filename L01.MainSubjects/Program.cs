4c7a286d54d7064d1a1d63dfa9815b969934a9c9
Zehra Seren <fatmazehraseren@gmail.com>
2024-09-29 12:49:00 +0300

    Lesson 1 is completed.

---
+++ L01.MainSubjects/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace L01.MainSubjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region PRINTING INSTRUCTIONS| YAZDIRMA KOMUTLARI
+            //Console.WriteLine("Hello World!");
+            //Console.Write("Hello");
+
+            //Console.WriteLine("***** Food Categories *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Soup");
+            //Console.WriteLine("2-Main Dishes");
+            //Console.WriteLine("3-Snacks");
+            //Console.WriteLine("4-Salad");
+            //Console.WriteLine("5-Desserts");
+            //Console.WriteLine("6-Drinks");
+            //Console.WriteLine();
+            //Console.WriteLine("***** Food Categories *****");
+            #endregion
+
+            #region STRING VARIABLES | STRING DEĞİŞKENLER
+            // Değişken_Türü Değişken_Adı;
+
+            //string name;
+            //name = "Zehra";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Zehra";
+            //customerSurname = "Şeren";
+            //customerPhone = "+90 521 234 5678";
+            //customerEmail = "fatmazehraseren@gmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+
+            //Console.WriteLine("***** Rezervasyon Kartı *****");
+            //Console.WriteLine();
+            //Console.WriteLine("---------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + district + "/" + city);
+            //Console.WriteLine("---------------------------------");
+
+            //Console.WriteLine();
+
+            //customerName = "Ayşegül";
+            //customerSurname = "Kaya";
+            //customerPhone = "+90 987 654 3210";
+            //customerEmail = "aysegulkaya@gmail.com";
+            //district = "Sapanca";
+            //city = "Sakarya";
+
+            //Console.WriteLine("---------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + district + "/" + city);
+            //Console.WriteLine("---------------------------------");
+
+            #endregion
+
+            #region INT(INTEGER) VARIABLES | INT(INTEGER) DEĞİŞKENLER
+
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            //int hamburgerPrice = 300;
+            //int cokePrice = 35;
+            //int waterPrice = 10;
+            //int friesPrice = 50;
+            //int pizzaPrice = 250;
+            //int lemonadePrice = 30;
+
+            //Console.WriteLine("Menü Fiyatı");
+            //Console.WriteLine();
+            //Console.WriteLine("---------------------------------");
+            //Console.WriteLine("Hamburger: " + hamburgerPrice + " TL");
+            //Console.WriteLine("Pizza: " + pizzaPrice + " TL");
+            //Console.WriteLine("Kızartma: " + friesPrice + " TL");
+            //Console.WriteLine("Cola: " + cokePrice + " TL");
+            //Console.WriteLine("Limonata: " + lemonadePrice + " TL");
+            //Console.WriteLine("Su: " + waterPrice + " TL");
+            //Console.WriteLine("---------------------------------");
+
+            //Console.WriteLine();
+
+            //int hamburgerCount;
+            //int cokeCount;
+            //int waterCount;
+            //int friesCount;
+            //int pizzaCount;
+            //int lemonadeCount;
+
+            //int totalHamburgerPrice;
+            //int totalCokePrice;
+            //int totalWaterPrice;
+            //int totalFriesPrice;
+            //int totalPizzaPrice;
+            //int totalLemonadePrice;
+
+            //hamburgerCount = 3;
+            //cokeCount = 3;
+            //waterCount = 3;
+            //friesCount = 1;
+            //pizzaCount = 0;
+            //lemonadeCount = 0;
+
+            //totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            //totalCokePrice = cokeCount * cokePrice;
+            //totalWaterPrice = waterCount * waterPrice;
+            //totalFriesPrice = friesCount * friesPrice;
+            //totalPizzaPrice = pizzaCount * pizzaPrice;
+            //totalLemonadePrice = lemonadeCount * lemonadePrice;
+
+            //Console.WriteLine("---------------------------------");
+            //Console.WriteLine("Hamburger Tutarı: " + totalHamburgerPrice + " TL");
+            //Console.WriteLine("Pizza Tutarı: " + totalPizzaPrice + " TL");
+            //Console.WriteLine("Kızartma Tutarı: " + totalFriesPrice + " TL");
+            //Console.WriteLine("Kola Tutarı: " + totalCokePrice + " TL");
+            //Console.WriteLine("Limonata Tutarı: " + totalLemonadePrice + " TL");
+            //Console.WriteLine("Su Tutarı: " + totalWaterPrice + " TL");
+            //Console.WriteLine("---------------------------------");
+
+            //Console.WriteLine();
+
+            //int totalPrice = totalHamburgerPrice + totalCokePrice + totalWaterPrice + totalFriesPrice + totalPizzaPrice + totalLemonadePrice;
+
+            //Console.WriteLine("Toplam Tutar: " + totalPrice + " TL");
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
---
+++ L01.MainSubjects/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L01.MainSubjects")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L01.MainSubjects")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("61a58a5f-e866-4b9e-bd44-0aeff2b253cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
